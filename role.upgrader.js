2d59ec948db6b04adbc84338a39a2bd37e79957c
Jack Edge <yellowbounder@gmail.com>
2016-07-21 13:53:38 +0100

    resupplying

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,1 +1,1 @@
-var roleUpgrader={run:function(a){0==a.carry.energy&&a.add_task({type:"harvest"});a.upgradeController(a.room.controller)==ERR_NOT_IN_RANGE&&a.moveTo(a.room.controller)}};module.exports=roleUpgrader;
+var roleUpgrader={run:function(a){0==a.carry.energy&&a.add_task({type:"resupply"});a.upgradeController(a.room.controller)==ERR_NOT_IN_RANGE&&a.moveTo(a.room.controller)}};module.exports=roleUpgrader;
