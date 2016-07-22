9bd35d798d483130ac51bce6416208528f4a1f8b
Jack Edge <yellowbounder@gmail.com>
2016-07-22 15:33:20 +0100

    mode mode

---
+++ role.tower.js
@@ -0,0 +1,1 @@
+module.exports={run:function(b){var a,c;a||(a=b.pos.findDamagedStructures(),a=b.pos.findClosestByRange(a),c="repair");a||(a=b.pos.findClosestByRange(FIND_HOSTILE_CREEPS),c="attack");a||(a=b.pos.findDamagedFriendlyCreeps(),a=b.pos.findClosestByRange(a),c="heal");a&&b.add_task({type:"tower_target",target_id:a.id,mode:c})}};
