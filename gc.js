1c1911985585a3f9980e588832a05a298bd26bba
Jack Edge <yellowbounder@gmail.com>
2016-07-23 17:39:32 +0100

    empty memory is empty

--- gc.js
+++ gc.js
@@ -1,1 +1,1 @@
-module.exports={gc:function(){for(var a in Memory.structures)Game.structures[a]||delete Memory.structures[a];for(var b in Memory.creeps)Game.creeps[b]||delete Memory.creeps[b]}};
+module.exports={gc:function(){for(var a in Memory.structures)Game.structures[a]||delete Memory.structures[a],Memory.structures[a]||delete Memory.structures[a];for(var b in Memory.creeps)Game.creeps[b]||delete Memory.creeps[b],Memory.creeps[a]||delete Memory.creeps[a]}};
