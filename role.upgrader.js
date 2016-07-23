116d7aac27cf5c68a4db0ace85485b68f05819e1
Jack Edge <yellowbounder@gmail.com>
2016-07-23 15:06:49 +0100

    Time to break everything

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,1 +1,1 @@
-var roleUpgrader={run:function(a){0==a.carry.energy&&a.add_task({type:"resupply"});a.upgradeController(a.room.controller)==ERR_NOT_IN_RANGE&&a.moveTo(a.room.controller)}};module.exports=roleUpgrader;
+var genericActions=require("genericActions"),roleUpgrader={name:"role.upgrader",run:function(a){0==a.carry.energy&&a.add_task({type:"resupply"});a.upgradeController(a.room.controller)==ERR_NOT_IN_RANGE&&a.moveTo(a.room.controller)}};module.exports=roleUpgrader;genericActions.register_task(roleUpgrader.name,roleUpgrader.run);
