504d3fe564bb52d02f8f2267efe8929d3bfa9ae4
Jack Edge <yellowbounder@gmail.com>
2016-07-28 09:32:19 +0100

    Okay, let's test

--- prototype.structuretower.js
+++ prototype.structuretower.js
@@ -1,1 +1,1 @@
-StructureTower.prototype.expected_power=function(a,b){switch(a){case "attack":break;case "heal":break;case "repair":break;default:throw"Unrecognised action type";}};
+StructureTower.prototype.expected_power=function(d,b){var a;switch(d){case "attack":a=TOWER_POWER_ATTACK;break;case "heal":a=TOWER_POWER_HEAL;break;case "repair":a=TOWER_POWER_REPAIR;break;default:throw"Unrecognised action type";}var e=TOWER_FALLOFF_RANGE,c=TOWER_FALLOFF;return b<=TOWER_OPTIMAL_RANGE?a:b>=e?a*(1-c):a-(b-TOWER_OPTIMAL_RANGE)*(a*c/(TOWER_FALLOFF_RANGE-TOWER_OPTIMAL_RANGE))};
