1fb1b77f9904eb4267b1a23358427ed5ac7b67c6
Jack Edge <yellowbounder@gmail.com>
2016-07-29 16:02:17 +0100

    support stuff

--- role.distantminer.js
+++ role.distantminer.js
@@ -1,1 +1,3 @@
-module.exports={run:function(a){}};
+var util=require("util");
+module.exports={name:"role.distantminer",run:function(a,c){"undefined"==typeof a.mode&&(a.mode="locate");return module.exports[a.mode](a,c)},locate:function(a,c){var b=Game.spawns[c.support];if(!b)return c.find_support(),{outcome:"rerun"};var e=b.room.roomName,b=util.room_walk(e,function(a,b){if(b==e)return!1;var d=Game.rooms[b];if(d)return d.find(FIND_SOURCES,{filter:function(a){return a.is_harvestable(c)}}).length;d=Memory.rooms[b].intel;if(!d)return!1;if(d.sources.length)return!0});return b.length?
+(a.target_room=b[0],a.mode="travel_to",{outcome:"rerun"}):{outcome:"continue"}},travel_to:function(a,c){}};require("task_manager").register(module.exports.name,module.exports.run);
