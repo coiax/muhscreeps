11c873918bf2ef6a01203c8adc9c2624029d8e95
Jack Edge <yellowbounder@gmail.com>
2016-08-03 16:33:55 +0100

    Mixins

---
+++ mixin.log.js
@@ -0,0 +1,1 @@
+var util=require("util");module.exports.log=function(a,b,d){var c=this.get_memory();c.log||(c.log=[]);a={level:a,message:d,tick:Game.time,subsystem:b};"undefined"!=typeof this.say&&this.say(b+":"+d);c.log.push(a);util.keep_n_values(c.log,20)};module.exports.warning=function(a,b){this.log("warning",a,b)};module.exports.error=function(a,b){this.log("error",a,b)};
