8e577ca81abc9d00fa85d219cdccdde7913c682b
Jack Edge <yellowbounder@gmail.com>
2016-08-08 17:36:43 +0100

    JS iteration2

--- role.builder.js
+++ role.builder.js
@@ -1,30 +1,26 @@
 var task_manager = require("task_manager"), outcomes = require("task_manager.globals").outcomes;
-function sort_construction_sites(f) {
-  return _.orderBy(f, function(b) {
+function sort_construction_sites(e) {
+  return _.orderBy(e, function(b) {
     return b.progressTotal - b.progress
   }, "desc")
 }
-function sort_structures(f) {
-  return _.sortBy(f, function(b) {
+function sort_structures(e) {
+  return _.sortBy(e, function(b) {
     return b.hits
   })
 }
-var roleBuilder = {name:"role.builder", run:function(f, b) {
+var roleBuilder = {name:"role.builder", run:function(e, b) {
   var a = Game.flags.Dismantle, c;
-  if(a) {
-    a.memory.assigned_creeps || (a.memory.assigned_creeps = []);
-    for(var e in a.memory.assigned_creeps) {
-      c = a.memory.assigned_creeps[e], Game.creeps[c] || (a.memory.assigned_creeps[c] = void 0)
-    }
-    c = a.memory.assigned_creeps.length
-  }
+  a && (a.memory.assigned_creeps || (a.memory.assigned_creeps = []), _.remove(a.memory.assigned_creeps, function(a) {
+    return!Game.creeps[a]
+  }), c = a.memory.assigned_creeps.length);
   if("undefined" != typeof c && 2 > c) {
     a.memory.assigned_creeps.push(b.name);
     if(!a.room) {
       return b.moveTo(a), new outcomes.InProgress
     }
-    if((e = a.pos.findInRange(FIND_STRUCTURES, 0)) && e.length) {
-      var d = {type:"dismantle", target_id:e[0].id};
+    if((c = a.pos.findInRange(FIND_STRUCTURES, 0)) && c.length) {
+      var d = {type:"dismantle", target_id:c[0].id};
       return new outcomes.PushTask(d)
     }
     a.remove()
@@ -33,13 +29,13 @@ var roleBuilder = {name:"role.builder", run:function(f, b) {
     return new outcomes.PushTask({type:"resupply"})
   }
   a = b.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-  e = sort_structures(b.room.find(FIND_MY_STRUCTURES, {filter:function(a) {
+  c = sort_structures(b.room.find(FIND_MY_STRUCTURES, {filter:function(a) {
     return a.structureType != STRUCTURE_RAMPART && a.hits < a.hitsMax
   }}));
-  c = sort_structures(b.room.find(FIND_STRUCTURES, {filter:function(a) {
+  var f = sort_structures(b.room.find(FIND_STRUCTURES, {filter:function(a) {
     return a.hits < a.hitsMax
   }}));
-  a ? d = a : e.length ? d = e[0] : c.length && (d = c[0]);
+  a ? d = a : c.length ? d = c[0] : f.length && (d = f[0]);
   if(d) {
     return d = {type:"construct", target_id:d.id}, new outcomes.PushTask(d)
   }
