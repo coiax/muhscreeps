3a19528f1160cd8194608d053f059dcc79df5d24
Jack Edge <yellowbounder@gmail.com>
2016-08-09 17:28:16 +0100

    Restructuring, no changes yet

--- role.distantminer.js
+++ role.distantminer.js
@@ -1,32 +1,22 @@
-var util = require("util"), task_manager = require("task_manager");
-module.exports = {name:"role.distantminer", run:function(a, c) {
-  "undefined" == typeof a.mode && (a.mode = "locate");
-  return module.exports[a.mode](a, c)
-}, locate:function(a, c) {
-  var b = Game.spawns[c.support];
-  if(!b) {
-    return c.find_support(), {outcome:"rerun"}
-  }
-  var e = b.room.roomName, b = util.room_walk(e, function(a, b) {
-    if(b == e) {
-      return!1
-    }
-    var d = Game.rooms[b];
-    if(d) {
-      return d.find(FIND_SOURCES, {filter:function(a) {
-        return a.is_harvestable(c)
+var util = require("util"), task_manager = require("task_manager"), outcomes = task_manager.globals.outcomes;
+module.exports.name = "role.distantminer";
+module.exports.run = function(c, d) {
+  var b = util.room_walk(d.room.name, function(c, b) {
+    var a = Game.rooms[b];
+    if(a) {
+      return a.find(FIND_SOURCES, {filter:function(a) {
+        return a.is_harvestable(d)
       }}).length
     }
-    d = Memory.rooms[b].intel;
-    if(!d) {
+    a = Memory.rooms[b].intel;
+    if(!a) {
       return!1
     }
-    if(d.sources.length) {
+    if(a.sources && a.sources.length) {
       return!0
     }
   });
-  return b.length ? (a.target_room = b[0], a.mode = "travel_to", {outcome:"rerun"}) : {outcome:"continue"}
-}, travel_to:function(a, c) {
-}};
+  return b.length ? (c.target_room = b[0], c.mode = "travel_to", {outcome:"rerun"}) : new outcomes.Failure("No suitable mining sites found.")
+};
 require("task_manager").register(module.exports.name, module.exports.run);
 
