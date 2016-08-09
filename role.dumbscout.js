afd661e9bef8acc8d03c73801e11364aaae999d0
Jack Edge <yellowbounder@gmail.com>
2016-08-09 18:44:49 +0100

    Y U NO WORK

--- role.dumbscout.js
+++ role.dumbscout.js
@@ -1,12 +1,19 @@
 var util = require("util"), task_manager = require("task_manager"), outcomes = task_manager.globals.outcomes;
-module.exports = {name:"role.dumbscout", parts:[[MOVE]], run:function(f, c) {
-  c.notifyWhenAttacked(!1);
-  for(var b = Game.map.describeExits(c.room.name), d, a;b.length && !a;) {
-    var e = _.sample(Object.keys(b));
-    d = b[e];
-    d == f.old_room && 1 != b.length || (delete b[e], a = parseInt(e), a = c.pos.findClosestByPath(a))
+module.exports = {name:"role.dumbscout", parts:[[MOVE]], run:function(c, d) {
+  d.notifyWhenAttacked(!1);
+  for(var g = d.room.name, a = Game.map.describeExits(g), e, b;a.length && !b;) {
+    var f = _.sample(Object.keys(a));
+    e = a[f];
+    if(!c.old_room || !(e == c.old_room && 1 != a.length)) {
+      delete a[f], b = parseInt(f), b = d.pos.findClosestByPath(b)
+    }
   }
-  return!a ? new outcomes.Failure("Cannot reach any exits.") : new outcomes.PushTask({type:"move_to_exit", destination:a, destination_room:d})
+  if(!b) {
+    return new outcomes.Failure("Cannot reach any exits.")
+  }
+  a = {type:"move_to_exit", destination:b, destination_room:e};
+  c.old_room = g;
+  return new outcomes.PushTask(a)
 }};
 require("task_manager").register(module.exports.name, module.exports.run);
 
