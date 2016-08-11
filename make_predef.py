89e96181d3bd8dadeccfc2f3a3fe67d30336e677
Jack Edge <yellowbounder@gmail.com>
2016-08-11 14:27:50 +0100

    Preperations for jshint

--- make_predef.py
+++ make_predef.py
@@ -1,11 +1,21 @@
-predef = ''
+import json
 
-first = True
-with open('globals') as f:
-    for line in f:
-        if not first:
-            predef += ","
-        first = False
-        predef += line.strip()
+def jslint():
+    predef = ''
 
-print(predef)
+    first = True
+    with open('globals') as f:
+        for line in f:
+            if not first:
+                predef += ","
+            first = False
+            predef += line.strip()
+
+    print(predef)
+
+def jshint():
+    with open('globals') as f:
+        L = []
+        for line in f:
+            L.append(line.strip())
+        print(json.dumps(L))
