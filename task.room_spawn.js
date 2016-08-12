8036cb77be8eaa3989076790422d25d010e9add3
Jack Edge <yellowbounder@gmail.com>
2016-08-12 20:36:44 +0100

    I see it's stripping the name

--- task.room_spawn.js
+++ task.room_spawn.js
@@ -1,6 +1,7 @@
-'use strict';var task_manager = require("task_manager"), outcomes = task_manager.outcomes, room_spawn = function(b, a) {
+'use strict';var task_manager = require("task_manager"), outcomes = task_manager.outcomes;
+function room_spawn(b, a) {
   console.log("Running task(room_spawn) for " + a);
   return new outcomes.NextTask
-};
+}
 task_manager.register(room_spawn);
 
